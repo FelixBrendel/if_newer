3d7aca19cd27b7f7a07a22b8b4ee49cdd94856df
Felix Brendel <felix.brendel@mvtec.com>
2025-08-07 17:35:43 +0200

    Updated the way script_dir is determined in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-pushd $SCRIPTPATH > /dev/null
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd $script_dir > /dev/null
 
 echo compiling
 cmake -S . -B .build && cmake --build .build || exit 1
